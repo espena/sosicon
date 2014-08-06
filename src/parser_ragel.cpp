103ac0019096ee8447f95051fa2fb97a8ad577a5
root <root@kursmaskin.(none)>
2014-08-06 15:30:29 +0200

    linux version

--- src/parser_ragel.cpp
+++ src/parser_ragel.cpp
@@ -193,7 +193,7 @@ _resume:
 			else if ( (*p) > *_mid )
 				_lower = _mid + 1;
 			else {
-				_trans += (_mid - _keys);
+				_trans += (unsigned int)(_mid - _keys);
 				goto _match;
 			}
 		}
@@ -216,7 +216,7 @@ _resume:
 			else if ( (*p) > _mid[1] )
 				_lower = _mid + 2;
 			else {
-				_trans += ((_mid - _keys)>>1);
+				_trans += (unsigned int)((_mid - _keys)>>1);
 				goto _match;
 			}
 		}
