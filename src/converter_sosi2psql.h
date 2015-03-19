10c686d2d6d0072c0ee196f44da93dc5fbd23b32
Espen Andersen <espen.andersen@nrk.no>
2015-03-19 22:54:30 +0100

    Updated source documentation.

--- src/converter_sosi2psql.h
+++ src/converter_sosi2psql.h
@@ -43,10 +43,10 @@ namespace sosicon {
         \addtogroup converters
         @{
     */
-    //! SOSI to PostGreSQL converter
+    //! SOSI to PostgreSQL/PostGIS converter
     /*!
         If command-line parameter -2psql is specified, this converter will handle the output
-        generation. Produces a PostGreSQL dump file from the SOSI source(s).
+        generation. Produces a PostgreSQL/PostGIS dump file from the SOSI source(s).
      */
     class ConverterSosi2psql : public IConverter {
 
@@ -68,67 +68,198 @@ namespace sosicon {
         RowsListCollection mRowsListCollection;
 
         //! Build SQL insert statements for all geometries
+        /*!
+            This function calls sosicon::ConverterSosi2psql::buildInsertStatement
+            for each of the WKT geometries types to export.
+            \param dbSchema String representing the name of the database schema.
+            \param dbTable String representing the base name of the database table.
+                           The name of the geometry for that table will be prepended
+                           to the base name.
+            \see sosicon::ConverterSosi2psql::buildInsertStatement()
+            \return The SQL insertion script content.
+        */
         std::string buildInsertStatements( std::string dbSchema,
                                            std::string dbTable );
 
         //! Build SQL insert statement for one geometry
+        /*!
+            Creates the SQL statements required to insert the data for one WKT
+            geometry.
+            \param wktGeom WKT geometry type for current insertion script.
+            \param dbSchema String representing the name of the database schema.
+            \param dbTable String representing the base name of the database table.
+                           The name of the geometry for that table will be prepended
+                           to the base name.
+            \see sosicon::ConverterSosi2psql::buildInsertStatements()
+            \return The SQL insertion script content.
+        */
         std::string buildInsertStatement( Wkt wktGeom,
                                           std::string dbSchema,
                                           std::string dbTable );
 
         //! Build SQL create statements for all geometries
+        /*!
+            This function calls sosicon::ConverterSosi2psql::buildCreateStatement
+            for each of the WKT geometries types to export.
+            \param dbSchema String representing the name of the database schema.
+            \param dbTable String representing the base name of the database table.
+                           The name of the geometry for that table will be prepended
+                           to the base name.
+            \see sosicon::ConverterSosi2psql::buildCreateStatement()
+            \return The SQL/DDL creation script content.
+        */
         std::string buildCreateStatements( std::string sridDest,
                                            std::string dbSchema,
                                            std::string dbTable );
 
-        //! Build SQL create statement for one geometry
+         //! Build SQL create statements for one geometry
+         /*!
+             This function calls sosicon::ConverterSosi2psql::buildCreateStatements
+             for each of the WKT geometries types to export.
+             \param dbSchema String representing the name of the database schema.
+             \param dbTable String representing the base name of the database table.
+                            The name of the geometry for that table will be prepended
+                            to the base name.
+             \see sosicon::ConverterSosi2psql::buildCreateStatements()
+             \return The SQL/DDL creation script content.
+         */
         std::string buildCreateStatement( Wkt wktGeom,
                                           std::string sridDest,
                                           std::string dbSchema,
                                           std::string dbTable );
 
         // Free all heap allocations
+        /*!
+            Release memory reserved for this converter. Called before destroying object.
+            It may not be necesary to invoke cleanup() if the program is about to
+            terminate anyway, as this can be quite time consuming after converting
+            large files.
+             \see sosicon::ConverterSosi2psql::cleanup( Wkt )
+        */
         void cleanup();
 
         // Free heap allocations for single geometry
+        /*!
+            Release memory reserved for one geometry. Called
+            from sosicon::ConverterSosi2psql::cleanup( Wkt )
+            \param wktGeom The WKT geometry type for which to delete allocated memory.
+            \see sosicon::ConverterSosi2psql::cleanup()
+        */
         void cleanup( Wkt wktGeom );
 
         //! Fetch element data fields recursively
+        /*!
+            Traverses the SOSI element tree recursively, extracting plain data fields.
+            The field names are stored in the hdr list, and the data values are stored
+            in the row list. The data size value associated wit each entry in hdr is
+            updated to reflect the longest encountered field length.
+            \param parent The SOSI (sub)tree to be traversed.
+            \param hdr The fields list (table header).
+            \param row The record set (table row).
+        */
         void extractData( ISosiElement* parent,
                           FieldsList& hdr,
                           std::map<std::string,std::string>*& row );
 
         //! Read current coordinate system from SOSI tree
+        /*!
+            Scans the SOSI header, searching for the KOORDSYS element with information
+            about the current coordinate system.
+            \param sosiTree Pointer to the root SOSI element.
+            \return The SRID code for the grid used in current file.
+        */
         std::string getSrid( ISosiElement* sosiTree );
 
         //! Convert curve geomery (sosi KURVE) to SQL export data
+        /*!
+            Extracts the coordinates from the given SOSI element and builds
+            a linestring WKT geometry from it.
+            \see sosicon::ConverterSosi2psql::insertPoint()
+            \see sosicon::ConverterSosi2psql::insertPolygon()
+            \param lineString SOSI geometry element (typically "KURVE").
+            \param sridSource Spatial reference grid ID for the source file.
+            \param sridDest Spatial reference grid ID for the target file.
+            \param geomField The name of the field within the recordset
+                             representing the geometry data.
+        */
         void insertLineString( ISosiElement* lineString,
                                std::string sridSource,
                                std::string sridDest,
                                std::string geomField );
 
         //! Convert single point geomery (sosi PUNKT) to SQL export data
+        /*!
+            Extracts the coordinate from the given SOSI element and builds
+            a point WKT geometry from it.
+            \see sosicon::ConverterSosi2psql::insertLineString()
+            \see sosicon::ConverterSosi2psql::insertPolygon()
+            \param point SOSI geometry element (typically "PUNKT" or "TEKST").
+            \param sridSource Spatial reference grid ID for the source file.
+            \param sridDest Spatial reference grid ID for the target file.
+            \param geomField The name of the field within the recordset
+                             representing the geometry data.
+        */
         void insertPoint( ISosiElement* point,
                           std::string sridSource,
                           std::string sridDest,
                           std::string geomField );
 
         //! Convert polygons (sosi FLATE) to SQL export data
+        /*!
+            Extracts the coordinates from the given SOSI element and builds
+            a polygon WKT geometry from it.
+            \see sosicon::ConverterSosi2psql::insertLineString()
+            \see sosicon::ConverterSosi2psql::insertPoint()
+            \param point SOSI geometry element (typically "FLATE").
+            \param sridSource Spatial reference grid ID for the source file.
+            \param sridDest Spatial reference grid ID for the target file.
+            \param geomField The name of the field within the recordset
+                             representing the geometry data.
+        */
         void insertPolygon( ISosiElement* polygon,
                             std::string sridSource,
                             std::string sridDest,
                             std::string geomField );
 
         //! Make SQL dump from SOSI tree
+        /*!
+            Iterates the SOSI tree once for each geometry to be exported, extracting
+            relevant elements and passing them on to one of the insertion routines.
+            \param sosiTree Pointer to the root SOSI element.
+            \param sridDest Spatial reference grid ID for the target file.
+            \param dbSchema String representing the name of the database schema.
+            \param dbTable String representing the base name of the database table.
+                           The name of the geometry for that table will be prepended
+                           to the base name.
+            \see sosicon::ConverterSosi2psql::insertPoint()
+            \see sosicon::ConverterSosi2psql::insertLineString()
+            \see sosicon::ConverterSosi2psql::insertPolygon()
+        */
         void makePsql( ISosiElement* sosiTree,
                        std::string sridDest,
                        std::string dbSchema,
                        std::string dbTable );
 
         //! Test if current element is filtered out by -t parameter
+        /*!
+            If the user uses the -t parameter to specify which OBJTYPE elements to
+            include in the export, this function tests if current element is opted
+            out of the export.
+            \param src SOSI element serch result to test.
+            \return True if current element should be excluded from the export file.
+        */
         bool objTypeExcluded( sosi::SosiElementSearch& src );
 
         //! Write SQL content
+        /*!
+            Assebles and prepares the SQL statements before writing them to the
+            destination file.
+            \param sridDest Spatial reference grid ID for the target file.
+            \param dbSchema String representing the name of the database schema.
+            \param dbTable String representing the base name of the database table.
+                           The name of the geometry for that table will be prepended
+                           to the base name.
+        */
         void writePsql( std::string sridDest,
                         std::string dbSchema,
                         std::string dbTable );
