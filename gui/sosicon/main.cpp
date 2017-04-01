764a4f32ede8d43e2a1c5959500e5834016a7b39
Espen Andersen <post@espenandersen.no>
2017-04-01 10:43:07 +0200

    Implement embedded font resources.

--- gui/sosicon/main.cpp
+++ gui/sosicon/main.cpp
@@ -1,10 +1,21 @@
 #include "mainfrm.h"
 #include <QApplication>
+#include <QResource>
+#include <QFont>
+#include <QFontDatabase>
 #include <memory>
 
 int main( int argc, char *argv[] )
 {
     QApplication app( argc, argv );
+    QResource::registerResource( "sosicon.rcc" );
+    QString fontPath = ":/fonts/DejaVuSansCondensed.ttf";
+    int fontId = QFontDatabase::addApplicationFont( fontPath );
+    if( fontId != -1 )
+    {
+        QFont font( "DejaVuSansCondensed" );
+        app.setFont( font );
+    }
     app.addLibraryPath( "INSTALL/extdll" );
     MainFrm mainFrm;
     mainFrm.show();
